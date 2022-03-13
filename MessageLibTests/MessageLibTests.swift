c785c1369b36bc22987207b48a362fb8e06e45e3
Artyom Vlasenko <loody.bot@icloud.com>
2022-03-13 20:23:10 +0300

    Test CI

---
+++ MessageLibTests/MessageLibTests.swift
@@ -0,0 +1,16 @@
+//
+//  MessageLibTests.swift
+//  MessageLibTests
+//
+//  Created by Artem Vlasenko on 13.03.2022.
+//  Copyright © 2022 Artem Vlasenko. All rights reserved.
+//
+
+import XCTest
+@testable import MessageLib
+
+class MessageLibTests: XCTestCase {
+  
+    
+
+}
